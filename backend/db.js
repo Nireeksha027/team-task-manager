83025b3a92789917f186d140f329349e4fc0283f
Nireeksha027 <nireekshakarkera8@gmail.com>
2026-05-06 21:42:02 +0530

    Connected Railway MySQL and production URLs

--- backend/db.js
+++ backend/db.js
@@ -1,18 +1,21 @@
 const mysql = require("mysql2");
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "Nireeksha_123",
-    database: "teamtask"
+const db = mysql.createConnection({
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT
 });
 
-connection.connect((err) => {
-    if (err) {
-        console.log("Database connection failed");
-    } else {
-        console.log("MySQL Connected");
-    }
+db.connect((err) => {
+
+  if (err) {
+    console.log("Database connection failed");
+    console.log(err);
+  } else {
+    console.log("MySQL Connected");
+  }
 });
 
-module.exports = connection;
+module.exports = db;
